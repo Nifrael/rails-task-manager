d3870c62d445bb1b7297f218a7e7123684f9f93f
Julien Marchand <nifrael66@gmail.com>
2024-01-29 12:37:39 +0100

    Task manager 1.0 without front

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "tasks", to: "tasks#list", as: :tasks
+  get "tasks/:id", to: "tasks#find", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  delete "tasks/:id", to: "tasks#delete", as: :delete_task
 
   # Defines the root path route ("/")
   # root "posts#index"
